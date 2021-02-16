74adb408c68a648ca206d75b746509dcd8a1bdd9
Taylor D. Gabatino <tgabatin@hawaii.edu>
2021-02-15 21:42:36 -1000

    Added basic info for the car makes

--- app/imports/api/userVehicles/UserVehicles.js
+++ app/imports/api/userVehicles/UserVehicles.js
@@ -9,21 +9,69 @@ class UserVehiclesCollection {
     //Sets the name of the Mongo Collection
     this.collection = new Mongo.Collection(this.name);
     this.schema = new SimpleSchema({
-      /**
-       * Should these all be separate databases, or should this all stay in one collection? Not
-       * too sure if the focus was going to primarily be on cars for now. If it is, I will add the information
-       * on the current collection of cars I found here today.
-       * Base info: mpg, cost, fuel capacity, electronic equivalent.
-       */
-      //Define the car database
-
-      //Define the bike database
-
-      //Define the walk database
-
-      //Define the carpool database
-
-      //Define the bus database
+    /**
+     * Car Database:
+     * Car Make, Model, MPG, Price, Subsidiary
+     */
+    //List of all of the car make (Dropdown for model will depend on selection of make)
+    carMake: {
+      type: String,
+      allowedValues: ['...',
+          'Acura',
+          'Alfa Romeo',
+          'Audi',
+          'BMW',
+          'Bentley',
+          'Buick',
+          'Cadillac',
+          'Chevrolet',
+          'Chrysler',
+          'Dodge',
+          'Fiat',
+          'Ford',
+          'GMC',
+          'Genesis',
+          'Honda',
+          'Hyundai',
+          'Infiniti',
+          'Jaguar',
+          'Jeep',
+          'Kia',
+          'Land Rover',
+          'Lexus',
+          'Lincoln',
+          'Lotus',
+          'Maserati',
+          'Mazda',
+          'Mercedes-Benz',
+          'Mercury',
+          'Mini',
+          'Mitsubishi',
+          'Nikola',
+          'Nissan',
+          'Polestar',
+          'Pontiac',
+          'Porsche',
+          'Ram',
+          'Rivian',
+          'Rolls-Royce',
+          'Saab',
+          'Saturn',
+          'Scion',
+          'Smart',
+          'Suzuki',
+          'Tesla',
+          'Toyota',
+          'Volkswagen',
+          'Volvo'
+      ],
+      //If no car model is chosen, the default parameter
+      defaultValue: '...',
+    },
+      //Car miles per gallon
+      carMPG: Number,
+      //Price of car
+      carPrice: Number
     });
     this.collection.attachSchema(this.schema);
     this.userPublicationName = `${this.name}.publication.user`;
