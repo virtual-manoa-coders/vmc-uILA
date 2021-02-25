6803b6866e238b1b693c49ea3a3e1c4851e26950
Edwin <ezheng@hawaii.edu>
2021-02-25 09:38:13 -1000

    Add mockup for trial converter, have landing page recognize logins

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Grid, Image, Button, Icon } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Grid, Button, Icon, Input, Dropdown } from 'semantic-ui-react';
+import { NavLink, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -14,22 +17,38 @@ class Landing extends React.Component {
                             Welcome to uILA
                         </div>
                     </div>
-                    <div>
-                        <Grid centered columns={4}>
-                            <Grid.Column textAlign='center'>
-                                <div>
-                                    Already a Member?
-                                </div>
-                                <Button inverted as={NavLink} exact to="/login" key='login'>Login</Button>
-                            </Grid.Column>
-                            <Grid.Column textAlign='center'>
-                                <div>
-                                    Not a Member?
-                                </div>
-                                <Button inverted as={NavLink} exact to="/login" key='signup'>Sign up</Button>
-                            </Grid.Column>
-                        </Grid>
-                    </div>
+                    {
+                        !this.props.currentUser &&
+                        <div>
+                            <Grid centered columns={4}>
+                                <Grid.Column textAlign='center'>
+                                    <div>
+                                        Already a Member?
+                                    </div>
+                                    <Button inverted as={NavLink} exact to="/login" key='login'>Login</Button>
+                                </Grid.Column>
+                                <Grid.Column textAlign='center'>
+                                    <div>
+                                        Not a Member?
+                                    </div>
+                                    <Button inverted as={NavLink} exact to="/login" key='signup'>Sign up</Button>
+                                </Grid.Column>
+                            </Grid>
+                        </div>
+                    }
+                    {
+                        this.props.currentUser &&
+                        <div>
+                            <Grid centered columns={3}>
+                                <Grid.Column textAlign='center'>
+                                    <div>
+                                        Get started
+                                    </div>
+                                    <Button inverted as={NavLink} exact to="/login" key='login'>Go to Dashboard</Button>
+                                </Grid.Column>
+                            </Grid>
+                        </div>
+                    }
                 </div>
             </div>
             <Grid className='description-section' textAlign='left'>
@@ -73,9 +92,47 @@ class Landing extends React.Component {
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
+            <div className={'trial-converter'}>
+                <Grid.Row>
+                    <h3>
+                        Try out our converter
+                    </h3>
+                </Grid.Row>
+                <Grid>
+                    <Grid.Row columns={4}>
+                        <Grid.Column width={8}>
+                            <div style={{ paddingTop: '1em', paddingBottom: '1em' }}>
+                                Distance Traveled
+                                <Input placeholder='How far did you travel?' />
+                            </div>
+                            <div>
+                                Method of Travel (Optional)
+                                <Dropdown
+                                    placeholder={'How did you travel?'}
+                                    selection
+                                />
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column width={8} verticalAlign={'middle'}>
+                            XX Lbs of Carbon Emitted
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
         </Grid>
     );
   }
 }
 
-export default Landing;
+/** Declare the types of all properties. */
+Landing.propTypes = {
+    currentUser: PropTypes.string,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+const LandingContainer = withTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+}))(Landing);
+
+/** Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter */
+export default withRouter(LandingContainer);
