2bc9a946e685a0685d2cf9c2e866a1dabee20722
s-rathyen <srathyen@hawaii.edu>
2021-02-25 11:21:43 -1000

    Preliminary updates to dashboard

--- app/imports/ui/pages/Dashboard.jsx
+++ app/imports/ui/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Divider, Segment, Header } from 'semantic-ui-react';
+import {Grid, Icon, Divider, Image, Segment, Header} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { UserTransportation } from '../../api/userData/UserTransportation';
@@ -10,40 +10,92 @@ import TransportPieChartWithDates from '../components/TransportPieChartWithDates
 class Dashboard extends React.Component {
   render() {
     return (
-        <Grid id='landing-page' verticalAlign='middle' container>
+        <Grid id='dash' padded verticalAlign='middle' container>
 
-            <Grid padded relaxed verticalAlign='middle'>
-                <Grid.Row id='space-row'>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={8} verticalAlign='middle'>
-                        Your monthly emission
-                        <Divider hidden/>
-                        <img src="/images/emission-example.png" className="ui fluid image"/>
-                    </Grid.Column>
-                    <Grid.Column width={8}>
-                        Monthly emission in Honolulu
-                        <Divider hidden/>
-                        <img src="/images/emission-example.png" className="ui fluid image"/>
-                    </Grid.Column>
-                </Grid.Row>
 
-              <Grid.Row id='space-row'>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <Segment>
-                    <Header style={{ fontFamily: 'Comfortaa' }} textAlign='center' as='h2'>Modes of Transportation</Header>
-                    <TransportPieChartWithDates userTransportation={this.props.userTransportation}/>
-                  </Segment>
-                </Grid.Column>
-              </Grid.Row>
+          <Grid textAlign='center' padded columns={4}>
+            <Grid.Row>
+              <Grid.Column>
+                <Divider hidden/>
+                <h1>Environmental Impact</h1>
+              </Grid.Column>
+            </Grid.Row>
 
-            </Grid>
-
+            <Grid.Row>
+              <Grid.Column verticalAlign='middle'>
+                <Icon name='huge arrow circle down'/>
+              </Grid.Column>
+              <Grid.Column textAlign='left'>
+                Green House Gas (GHG) Reduced<br/>
+                <h2># lbs</h2>
+              </Grid.Column>
+            </Grid.Row>
             <Divider hidden/>
 
+            <Grid.Row>
+              <Grid.Column verticalAlign='middle'>
+                <Icon name='huge taxi'/>
+              </Grid.Column>
+              <Grid.Column textAlign='left'>
+                Vehicle Miles Traveled (VMT) Reduced<br/>
+                <h2># miles</h2>
+              </Grid.Column>
+            </Grid.Row>
+            <Divider hidden/>
+
+            <Grid.Row>
+              <Grid.Column verticalAlign='middle'>
+                <Icon name='huge road'/>
+              </Grid.Column>
+              <Grid.Column textAlign='left'>
+                Gallons of Gas Saved<br/>
+                <h2># gallons</h2>
+              </Grid.Column>
+            </Grid.Row>
+            <Divider hidden/>
+
+            <Grid.Row>
+              <Grid.Column verticalAlign='middle' >
+                <Icon name='huge home'/>
+              </Grid.Column>
+              <Grid.Column textAlign='left'>
+                Days worked at home<br/>
+                <h2># days</h2>
+              </Grid.Column>
+            </Grid.Row>
+            <Divider hidden/>
+
+
+            <Grid.Row>
+              <Grid.Column verticalAlign='middle'>
+                <Icon name='huge bicycle'/>
+              </Grid.Column>
+              <Grid.Column textAlign='left'>
+                Days biked to work<br/>
+                <h2># days</h2>
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row id='space-row'>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Segment>
+                  <Header style={{ fontFamily: 'Comfortaa' }} textAlign='center' as='h2'>Modes of Transportation</Header>
+                  <TransportPieChartWithDates userTransportation={this.props.userTransportation}/>
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+
+          </Grid>
+
+
+
+
         </Grid>
+
+
+
     );
   }
 }
