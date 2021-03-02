06920f0c66d0b7c2d0b4b1188cff49acfa6d0301
angcylau <angelacy@hawaii.edu>
2021-03-02 10:02:26 -1000

    Functioning GHG calculator added to Landing page
    Still needs work.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,78 +1,90 @@
 import React from 'react';
-import { Grid, Image, Button, Icon } from 'semantic-ui-react';
+import { Grid, Button, Icon } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import GhgCalculator from '../components/GhgCalculator';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-            <div className='banner-image'>
-                <div className='banner-image-row'>
-                    <div className='welcome-text'>
-                        <div>
-                            How did YOU impact Hawai'i today?
-                        </div>
-                    </div>
-                    <div>
-                        <Grid centered columns={4}>
-                            <Grid.Column textAlign='center'>
-                                <div>
-                                    Already a Member?
-                                </div>
-                                <Button inverted as={NavLink} exact to="/login" key='login'>Login</Button>
-                            </Grid.Column>
-                            <Grid.Column textAlign='center'>
-                                <div>
-                                    Not a Member?
-                                </div>
-                                <Button inverted as={NavLink} exact to="/login" key='signup'>Sign up</Button>
-                            </Grid.Column>
-                        </Grid>
-                    </div>
+          <div className='banner-image'>
+            <div className='banner-image-row'>
+              <div className='welcome-text'>
+                <div>
+                  How did YOU impact Hawai'i today?
                 </div>
+              </div>
+              <div>
+                <Grid centered columns={4}>
+                  <Grid.Column textAlign='center'>
+                    <div>
+                      Already a Member?
+                    </div>
+                    <Button inverted as={NavLink} exact to="/login" key='login'>Login</Button>
+                  </Grid.Column>
+                  <Grid.Column textAlign='center'>
+                    <div>
+                      Not a Member?
+                    </div>
+                    <Button inverted as={NavLink} exact to="/login" key='signup'>Sign up</Button>
+                  </Grid.Column>
+                </Grid>
+              </div>
             </div>
-            <Grid className='description-section' textAlign='left'>
-                <Grid.Row className='description-header'>
-                    What does Project Malama offer
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={2}>
-                        <Icon name='handshake outline'/>
-                        <br/>
-                        <div>
-                            Connect
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        Connect with the community and see how you are helping reduce emissions.
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={2}>
-                        <Icon name='clipboard outline'/>
-                        <br/>
-                        <div>
-                            Track
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        Log in your distance traveled alongside your method of transportation to track your emissions
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={2}>
-                        <Icon name='sliders horizontal'/>
-                        <br/>
-                        <div>
-                            Compare
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={12}>
-                        Compare your current emission options to other options
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+          </div>
+          <Grid className='description-section' textAlign='left'>
+            <Grid.Row className='description-header'>
+              What does Project Malama offer
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={2}>
+                <Icon name='handshake outline'/>
+                <br/>
+                <div>
+                  Connect
+                </div>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                Connect with the community and see how you are helping reduce emissions.
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={2}>
+                <Icon name='clipboard outline'/>
+                <br/>
+                <div>
+                  Track
+                </div>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                Log in your distance traveled alongside your method of transportation to track your emissions
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={2}>
+                <Icon name='sliders horizontal'/>
+                <br/>
+                <div>
+                  Compare
+                </div>
+              </Grid.Column>
+              <Grid.Column width={12}>
+                Compare your current emission options to other options
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Grid container>
+            <Grid.Row centered className='ghg-landing'>
+              Not ready to become a member yet? Test out the emissions calculator here!
+            </Grid.Row>
+            <Grid.Row className='about-font' centered style={{ fontSize: '0.75em' }}>
+              Enter your vehicle&apos;s MPG and distance traveled to calculate the amount of CO2 emitted.
+            </Grid.Row>
+            <Grid.Row>
+              <GhgCalculator/>
+            </Grid.Row>
+          </Grid>
         </Grid>
     );
   }
