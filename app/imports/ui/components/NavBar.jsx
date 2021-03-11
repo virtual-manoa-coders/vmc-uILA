65e19b4af740d82dd29d1254e7045353ee19c7d9
angcylau <angelacy@hawaii.edu>
2021-03-11 05:41:22 -1000

    Removed About from NavBar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,8 +16,7 @@ class NavBar extends React.Component {
           <Image size='small' src='images/logo2.png'/>
         </Menu.Item>
         {this.props.currentUser ? (
-            [<Menu.Item as={NavLink} activeClassName="active" exact to="/about" key='about'>About</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/comparator" key='comparator'>Comparator</Menu.Item>,
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/comparator" key='comparator'>Comparator</Menu.Item>,
                 <Menu.Item as={NavLink} activeClassName="active" exact to="/community" key='community'>Community</Menu.Item>,
                 <Menu.Item as={NavLink} activeClassName="active" exact to="/dashboard" key="dashboard">Dashboard</Menu.Item>]
         ) : ''}
