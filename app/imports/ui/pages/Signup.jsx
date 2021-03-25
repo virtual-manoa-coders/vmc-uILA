6ad93a8cca045c3fd7341527dc2562da976b18af
Taylor Gabatino <tgabatin@hawaii.edu>
2021-03-24 23:32:36 -0700

    Added ability to choose car make and year on signup page and add it to meateor profile

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -4,7 +4,20 @@ import { Link, Redirect } from 'react-router-dom';
 import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
 import { UserInfo } from '../../api/userData/UserInfo';
+import { UserVehicles } from '../../api/userVehicles/UserVehicles';
+import { Roles } from 'meteor/alanning:roles';
 
+function addProfile({ name, email, image, carMake, carModel, carYear, carMPG, CO2Reduced, VMTReduced, fuelSaved }) {
+  const createdAt = new Date();
+  const carID = UserVehicles.collection.find( ).fetch().filter(car => car.carModel === carModel && car.carYear === carYear)[0]._id;
+  console.log(carID);
+  if (carID) {
+    console.log(` Defining profile: ${email} with car: ${carYear} ${carModel} carID: ${carID}`);
+    UserInfo.collection.insert({ name: name, email: email, image: image, carID: carID, createdAt: createdAt });
+  } else {
+    console.log(` Unable to define ${email} with car ${carYear} ${carModel}`);
+  }
+}
 /**
  * Signup component is similar to signin component, but we create a new user instead.
  */
@@ -12,14 +25,13 @@ class Signup extends React.Component {
   /** Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', confirmPassword: '', error: '', errorConfirm: false, redirectToReferer: false };
+    this.state = { email: '', password: '', confirmPassword: '', carMake: '', carYear: '', error: '', errorConfirm: false, redirectToReferer: false };
   }
 
   /** Update the form controls each time the user interacts with them. */
   handleChange = (e, { name, value }) => {
     this.setState({ [name]: value });
   }
-
   /*
   TODO:
   - Create the  UserData Database
@@ -29,11 +41,11 @@ class Signup extends React.Component {
 
   /** Handle Signup submission. Create user account and a profile entry, then redirect to the home page. */
   submit = () => {
-    const { email, password, confirmPassword } = this.state;
+    const { email, password, confirmPassword, carMake, carYear } = this.state;
     if (password !== confirmPassword) {
       this.setState({ error: 'Passwords do not match, try again' });
     } else {
-      Accounts.createUser({ email, username: email, password }, (err) => {
+      Accounts.createUser({ email, username: email, password, profile: {carMake, carYear}}, (err) => {
         if (err) {
           this.setState({ error: err.reason });
         } else {
@@ -47,7 +59,7 @@ class Signup extends React.Component {
           //   userID,
           //   informationEntered,
           // }));
-          UserInfo.collection.insert({ email });
+          UserInfo.collection.insert({_id: Meteor.userId(), email :email,  });
         }
       });
     }
@@ -98,6 +110,28 @@ class Signup extends React.Component {
                       placeholder="Confirm Password"
                       type="password"
                       onChange={this.handleChange}
+                  />
+                  <Form.Select
+                      fluid
+                      label='Car Make'
+                      name='carMake'
+                      options={UserVehicles.cmAllowedValues.map(function(currentValue, index, array){
+                          return {key:index, text: currentValue, value: currentValue};
+                        })
+                      }
+                      placeholder='Car Make'
+                      onChange={this.handleChange}
+                  />
+                  <Form.Select
+                    fluid
+                    label='Car Year'
+                    name='carYear'
+                    options={UserVehicles.cyAllowedValues.map(function(currentValue, index, array){
+                        return {key:index, text:currentValue, value:currentValue};
+                      })
+                    }
+                    placeholder='Car Year'
+                    onChange={this.handleChange}
                   />
                   <Form.Button id="signup-form-submit" content="Submit"/>
                 </Segment>
